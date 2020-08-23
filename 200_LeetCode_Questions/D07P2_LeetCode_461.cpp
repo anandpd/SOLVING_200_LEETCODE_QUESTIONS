791408efd312509dcf03bdae3ca32286ef72c77d
Anand <danand407@gmail.com>
2020-08-23 19:00:37 +0530

    Update D07P2_LeetCode_461.cpp

--- 200_LeetCode_Questions/D07P2_LeetCode_461.cpp
+++ 200_LeetCode_Questions/D07P2_LeetCode_461.cpp
@@ -1,1 +1,13 @@
-# C++ code coming soon
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+    int xor_num = x^y;
+    int counter=0;
+    while(xor_num > 0){
+        xor_num = (xor_num & (xor_num-1));
+        counter++;
+    }
+    return counter;
+        
+    }
+};
