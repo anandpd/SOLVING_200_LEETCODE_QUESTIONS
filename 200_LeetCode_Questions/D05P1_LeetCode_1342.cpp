5790f5f2595d8a640ff0e41f599a7b4af4bbc1fc
Anand <danand407@gmail.com>
2020-08-23 18:59:35 +0530

    Update D05P1_LeetCode_1342.cpp

--- 200_LeetCode_Questions/D05P1_LeetCode_1342.cpp
+++ 200_LeetCode_Questions/D05P1_LeetCode_1342.cpp
@@ -1,1 +1,16 @@
-# C++ code coming soon
+class Solution{
+public:
+  int numberOfSteps (int num){
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    else{
+	  int dp[num + 1];
+	  dp[0] = dp[1] = 1;
+    for (int i = 2; i <= num; i++){
+	    if (i % 2 == 0)   dp[i] = dp[i / 2] + 1;
+	    else if (i % 2 != 0) dp[i] = dp[i - 1] + 1;    
+	       }
+	return dp[num];
+      }
+   }
+};
